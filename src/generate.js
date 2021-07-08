51cc3b36a58d1a3a9393a5274fff61cdd2c9704b
GiorgiMskhiladze99 <g.mskhiladze99@gmail.com>
2021-07-08 20:20:12 +0400

    make responsive all device

---
+++ src/generate.js
@@ -0,0 +1,17 @@
+//select buttons, inputs and output
+const output = document.getElementById('result');
+const copyBtn = document.getElementById('copy');
+const sliderValue = document.getElementById('slider-val');
+
+const lowerCase = document.getElementById('lowercase');
+const upperCase = document.getElementById('uppercase');
+const numbers = document.getElementById('numbers');
+const symbols = document.getElementById('symbols');
+
+const generateBtn = document.getElementById('generate');
+
+// arrays
+const numbers_arr = "0123456789";
+const symbols_arr = "!@#$%^&*_-+=";
+const alpha_lower = "abcdefghijklmnopqrstuvwxyz";
+const alpha_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
